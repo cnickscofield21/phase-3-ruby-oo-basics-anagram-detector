bf2f0ee1283487e708ee16fd68e67648f8cfcb9a
Nick Scofield <c.nick.scofield@gmail.com>
2023-01-31 15:40:48 -0700

    commit tree 750cd30021ff383ad871d933d79fd48c6cffc5e6

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+require "pry"
+
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word= word
+    end
+
+    def match(word_arr)
+        word_arr.select {|w| w.chars.sort == @word.chars.sort}
+    end
+end
